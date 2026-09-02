4797a50ab65c0a5177761c3b98d6c4959d5c66ae
Komal Chouhan <komalchouhan1554@gmail.com>
2026-09-02 17:46:51 +0530

    Named Queries in JPA

---
+++ G Spring with JPA (ORM - 2)/d. Named Queries in JPA/b JPA Named Queries/src/main/java/in/entity/Student.java
@@ -0,0 +1,62 @@
+package in.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="std_details")
+@NamedQuery(name="getAllStdDetails",query="SELECT s FROM Student s")
+@NamedQuery(name="getStdDetailsById",query="SELECT s FROM Student s WHERE s.id = :stdId")
+public class Student 
+{
+	@Id
+	@Column(name="std_id")
+	private int id;
+	
+	@Column(name="std_name")
+	private String name;
+	
+	@Column(name="std_rollno")
+	private int rollno;
+	
+	@Column(name="std_marks")
+	private float marks;
+
+	// Setters and getters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getRollno() {
+		return rollno;
+	}
+
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+
+	public float getMarks() {
+		return marks;
+	}
+
+	public void setMarks(float marks) {
+		this.marks = marks;
+	}
+	
+	
+}
---
+++ G Spring with JPA (ORM - 2)/d. Named Queries in JPA/b JPA Named Queries/src/main/java/in/main/App.java
@@ -0,0 +1,86 @@
+package in.main;
+
+import java.util.List;
+import in.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
+public class App 
+{
+    public static void main(String[] args) 
+    {
+    		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("main-persistence-unit");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		try {
+			
+			TypedQuery<Student> query = entityManager.createNamedQuery("getAllStdDetails",Student.class);
+			List<Student> std_lst = query.getResultList();
+
+			for (Student std : std_lst) 
+			{
+				System.out.println("--------------------------");
+				System.out.println("Id: " + std.getId());
+				System.out.println("Name: " + std.getName());
+				System.out.println("Roll NO: " + std.getRollno());
+				System.out.println("Marks: " + std.getMarks());
+			}
+			
+			// ---------------------------------------------------
+			
+			TypedQuery<Student> query2 = entityManager.createNamedQuery("getStdDetailsById",Student.class);
+			query2.setParameter("stdId", 3);
+			Student std2 = query2.getSingleResult();
+			
+			System.out.println("Hello "+ std2.getName() +", You got "+ std2.getMarks()+"% marks ⚔️⚔️⚔️");
+			
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+			
+			System.out.println("Failed !!!");
+		}
+		finally 
+		{
+			entityManager.close();
+			entityManagerFactory.close();
+		}
+
+    }
+}
+
+// Output:-
+
+/*
+ 
+
+Hibernate: select s1_0.std_id,s1_0.std_marks,s1_0.std_name,s1_0.std_rollno from std_details s1_0
+--------------------------
+Id: 1
+Name: Vishal Jadhav
+Roll NO: 101
+Marks: 98.7
+--------------------------
+Id: 2
+Name: Priya Patel
+Roll NO: 102
+Marks: 92.0
+--------------------------
+Id: 3
+Name: Amit Verma
+Roll NO: 103
+Marks: 78.2
+--------------------------
+Id: 4
+Name: Sneha Reddy
+Roll NO: 104
+Marks: 88.0
+
+Hibernate: select s1_0.std_id,s1_0.std_marks,s1_0.std_name,s1_0.std_rollno from std_details s1_0 where s1_0.std_id=?
+
+Hello Amit Verma, You got 78.2% marks ⚔️⚔️⚔️
+
+ */
